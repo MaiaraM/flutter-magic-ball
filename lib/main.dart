0eaafa1a004c0fbe303d25395b581df8281bdee5
Maiara <maiara.martins@stoom.com.br>
2020-03-28 14:18:27 -0300

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+          ),
+          body: MagicBallPage(),
+        ),
       ),
     );
+
+class MagicBallPage extends StatefulWidget {
+  @override
+  _MagicBallPageState createState() => _MagicBallPageState();
+}
+
+class _MagicBallPageState extends State<MagicBallPage> {
+  int number = 3;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  number = Random().nextInt(5) + 1;
+                });
+              },
+              child: Image.asset('images/ball$number.png'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
